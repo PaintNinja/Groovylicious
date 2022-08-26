8c32789fd7f82a9927cd28f18a7c6d612122d663
Matyrobbrt <matyrobbrt@gmail.com>
2022-08-27 00:36:07 +0300

    Docs.

--- src/transform/groovy/ga/ozli/minecraftmods/groovylicious/transform/registroid/recipetype/RecipeTypeAddon.groovy
+++ src/transform/groovy/ga/ozli/minecraftmods/groovylicious/transform/registroid/recipetype/RecipeTypeAddon.groovy
@@ -5,7 +5,21 @@ import groovy.transform.CompileStatic
 
 import java.lang.annotation.*
 
-@Documented // TODO docs
+/**
+ * An {@link ga.ozli.minecraftmods.groovylicious.transform.registroid.RegistroidAddon Registroid addon} which makes
+ * all {@linkplain net.minecraft.world.item.crafting.RecipeType RecipeType} fields within the class be created using
+ * {@linkplain net.minecraft.world.item.crafting.RecipeType#simple(net.minecraft.resources.ResourceLocation)}, with the
+ * name being the registry name of the RecipeType field. You do <strong>not</strong> need to initialise the field. <br> <br>
+ * Example in:
+ * <pre>
+ * {@code static final RecipeType<MyRecipe> HELLO_WORLD}
+ * </pre>
+ * Example out:
+ * <pre>
+ * {@code static final RecipeType<MyRecipe> HELLO_WORLD = RecipeType.simple(new ResourceLocation(yourModId, 'hello_world'))}
+ * </pre>
+ */
+@Documented
 @CompileStatic
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.SOURCE)
--- src/transform/groovy/ga/ozli/minecraftmods/groovylicious/transform/registroid/sound/SoundEventAddon.groovy
+++ src/transform/groovy/ga/ozli/minecraftmods/groovylicious/transform/registroid/sound/SoundEventAddon.groovy
@@ -6,7 +6,22 @@ import groovy.transform.CompileStatic
 
 import java.lang.annotation.*
 
-@Documented // TODO docs
+/**
+ * An {@link ga.ozli.minecraftmods.groovylicious.transform.registroid.RegistroidAddon Registroid addon} which makes
+ * all {@linkplain net.minecraft.sounds.SoundEvent SoundEvent} fields within the class be created using
+ * the correct deduced registry name. You do <strong>not</strong> need to initialise the field. <br><br>
+ * Example in:
+ * <pre>
+ * {@code static final SoundEvent HELLO_WORLD}
+ * {@code static final SoundEvent HELLO_THERE = new SoundEvent(null, 0.12f)}
+ * </pre>
+ * Example out:
+ * <pre>
+ * {@code static final SoundEvent HELLO_WORLD = new SoundEvent(new ResourceLocation(yourModId, 'hello_world'))}
+ * {@code static final SoundEvent HELLO_THERE = new SoundEvent(new ResourceLocation(yourModId, 'hello_there'), 0.12f)}
+ * </pre>
+ */
+@Documented
 @CompileStatic
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.SOURCE)
